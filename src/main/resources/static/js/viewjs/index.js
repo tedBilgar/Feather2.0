153250d2ce4ca5383db22a847c19500160c3c7b4
Denis Urakanov <mcquicklyss@gmail.com>
2019-04-07 13:51:35 +0500

    api begin adding

--- src/main/resources/static/js/viewjs/index.js
+++ src/main/resources/static/js/viewjs/index.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Привет, Vue!'
-    }
-})
+        el: '#app',
+        data () {
+            return {
+                info: null
+            }
+        },
+        mounted () {
+            axios
+                .get('http://localhost:9000/rest/user-desk/user/1')
+                .then(response => (this.info = response.data))
+        }
+    });
